e7bd42c2dbe6d14d424b2238ca80d0f036b16d6d
g <you@example.com>
2025-02-17 10:05:09 +0100

    init

---
+++ hello.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def main():
+    print("Hello from python-demo!")
+    df = pd.DataFrame({'Message': ['Hello, World!']})
+
+    # Print the DataFrame
+    print(df)
+
+if __name__ == "__main__":
+    main()
